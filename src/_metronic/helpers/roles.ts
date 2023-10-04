c62156cd4b70fd04bbd7f534f82a1c035ea65ecc
kenez.megyeri <m.kenz99@gmail.com>
2023-10-04 23:57:51 +0200

    Role based sideBar

---
+++ src/_metronic/helpers/roles.ts
@@ -0,0 +1,3 @@
+export const ADMINISTRATOR_ROLE = 'Administrator'
+export const SYSTEM_ROLE = 'System'
+export const SUPERVISOR_ROLE = 'Supervisor'
--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -1,7 +1,11 @@
 import {AsideMenuItemWithSub} from './AsideMenuItemWithSub'
 import {AsideMenuItem} from './AsideMenuItem'
+import {useSelector} from 'react-redux'
+import {userRolesSelector} from '../../../../app/modules/auth/core/authSelector'
+import {ADMINISTRATOR_ROLE} from '../../../helpers/roles'
 
 export function AsideMenuMain() {
+  const userRole: string[] = useSelector(userRolesSelector)
   return (
     <>
       <AsideMenuItem to='/dashboard' icon='black-right' title='Menü' fontIcon='bi-app-indicator' />
@@ -10,30 +14,36 @@ export function AsideMenuMain() {
           <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Kusza</span>
         </div>
       </div>
-      <AsideMenuItemWithSub
-        to='/crafted/pages'
-        title='Árajánlatok'
-        fontIcon='bi-archive'
-        icon='black-right'
-      >
-        <AsideMenuItem to='/administrator/createnewbid' title='Új árajánlat' hasBullet={true} />
+      {userRole.includes(ADMINISTRATOR_ROLE) && (
         <AsideMenuItemWithSub
-          to='/crafted/accounts'
-          title='Árajánlataim'
+          to='/crafted/pages'
+          title='Árajánlatok'
+          fontIcon='bi-archive'
           icon='black-right'
-          fontIcon='bi-person'
         >
-          <AsideMenuItem to='/crafted/account/overview' title='Összes' hasBullet={true} />
-          <AsideMenuItem to='/crafted/account/settings' title='Elfogadott' hasBullet={true} />
-          <AsideMenuItem to='/crafted/account/settings' title='Elutasitott' hasBullet={true} />
-          <AsideMenuItem to='/crafted/account/settings' title='Elfogadásra váró' hasBullet={true} />
-          <AsideMenuItem
-            to='/crafted/account/settings'
-            title='Szerkesztés alatt'
-            hasBullet={true}
-          />
+          <AsideMenuItem to='/administrator/createnewbid' title='Új árajánlat' hasBullet={true} />
+          <AsideMenuItemWithSub
+            to='/crafted/accounts'
+            title='Árajánlataim'
+            icon='black-right'
+            fontIcon='bi-person'
+          >
+            <AsideMenuItem to='/crafted/account/overview' title='Összes' hasBullet={true} />
+            <AsideMenuItem to='/crafted/account/settings' title='Elfogadott' hasBullet={true} />
+            <AsideMenuItem to='/crafted/account/settings' title='Elutasitott' hasBullet={true} />
+            <AsideMenuItem
+              to='/crafted/account/settings'
+              title='Elfogadásra váró'
+              hasBullet={true}
+            />
+            <AsideMenuItem
+              to='/crafted/account/settings'
+              title='Szerkesztés alatt'
+              hasBullet={true}
+            />
+          </AsideMenuItemWithSub>
         </AsideMenuItemWithSub>
-      </AsideMenuItemWithSub>
+      )}
     </>
   )
 }
