623062f2daa4a093be8f4ead807535eccf7de0bc
kenez.megyeri <m.kenz99@gmail.com>
2023-10-24 20:16:33 +0200

    [16] Remove unnecessary pages and routes

--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -2,17 +2,11 @@ import {Navigate, Route, Routes} from 'react-router-dom'
 import {MasterLayout} from '../../_metronic/layout/MasterLayout'
 import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
 import {MenuTestPage} from '../pages/MenuTestPage'
-import BuilderPageWrapper from '../pages/layout-builder/BuilderPageWrapper'
 import {lazy} from 'react'
 
 const PrivateRoutes = () => {
   const CreateNewBid = lazy(() => import('../pages/administrator/CreateNewBid/CreateNewBidPage'))
   const ListAllBid = lazy(() => import('../pages/administrator/ListBids/ListAllBid'))
-  // const WizardsPage = lazy(() => import('../modules/wizards/WizardsPage'))
-  // const AccountPage = lazy(() => import('../modules/accounts/AccountPage'))
-  // const WidgetsPage = lazy(() => import('../modules/widgets/WidgetsPage'))
-  // const ChatPage = lazy(() => import('../modules/apps/chat/ChatPage'))
-  // const UsersPage = lazy(() => import('../modules/apps/user-management/UsersPage'))
 
   return (
     <Routes>
@@ -21,7 +15,6 @@ const PrivateRoutes = () => {
         <Route path='auth/*' element={<Navigate to='/dashboard' />} />
         {/* Pages */}
         <Route path='dashboard' element={<DashboardWrapper />} />
-        <Route path='builder' element={<BuilderPageWrapper />} />
         <Route path='menu-test' element={<MenuTestPage />} />
         {/* Lazy Modules */}
         <Route path='administrator/createnewbid' element={<CreateNewBid />} />
