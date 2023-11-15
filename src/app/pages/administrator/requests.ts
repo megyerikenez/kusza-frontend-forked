d7b01c8d8c84468a991aaadd4e603cab67e42bc4
kenez.megyeri <m.kenz99@gmail.com>
2023-11-15 20:11:27 +0100

    [23] Cherry pick and Supervisor requests

--- src/app/pages/administrator/requests.ts
+++ src/app/pages/administrator/requests.ts
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 const API_URL = process.env.REACT_APP_API_URL
 
+export const GET_ALL_BIDS = `${API_URL}/orderconfirmation/getall`
 export const GET_BID_BY_USERID_URL = `${API_URL}/orderconfirmation/getbyuserid`
 export const POST_NEW_BID_URL = `${API_URL}/orderconfirmation/create`
-export const PUT_NEXT_STATUS_URL = `${API_URL}/orderconfirmation/movetonextstatus`
+export const PUT_NEXT_STATUS_URL = `${API_URL}/orderconfirmation/movetomovetonextstatus`
 export const DELETE_BID_URL = `${API_URL}/orderconfirmation/delete`
 
 export function getUserBids() {
--- src/app/pages/supervisor/requests.ts
+++ src/app/pages/supervisor/requests.ts
@@ -1,9 +1,8 @@
+import {GET_ALL_BIDS, PUT_NEXT_STATUS_URL} from '../administrator/requests'
 import axios from 'axios'
 
 const API_URL = process.env.REACT_APP_API_URL
-
-export const GET_ALL_BIDS = `${API_URL}/orderconfirmation/getall`
-export const PUT_NEXT_STATUS_URL = `${API_URL}/orderconfirmation/nextstatus`
+const PUT_DECLINE_BID_URL = `${API_URL}/orderconfirmation/decline`
 
 export const getAllBids = () => {
   return axios.get(GET_ALL_BIDS)
@@ -11,4 +10,10 @@ export const getAllBids = () => {
 
 export const nextStatus = (id: number) => {
   return axios.put(`${PUT_NEXT_STATUS_URL}/${id}`)
+}
+
+export const declineBid = (id: number, reason: string) => {
+  return axios.put(`${PUT_DECLINE_BID_URL}/${id}`, {
+    reason: reason,
+  })
 }
