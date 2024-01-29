a08bbfba384725df9aaaea21c6c91fb36dbe4a20
kenez.megyeri <m.kenz99@gmail.com>
2024-01-29 20:49:45 +0100

    [32] Toaster for Notifications without styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import {Outlet} from 'react-router-dom'
 import {I18nProvider} from '../_metronic/i18n/i18nProvider'
 import {LayoutProvider, LayoutSplashScreen} from '../_metronic/layout/core'
 import {MasterInit} from '../_metronic/layout/MasterInit'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
   return (
     <Suspense fallback={<LayoutSplashScreen />}>
+      <Toaster />
       <I18nProvider>
         <LayoutProvider>
           <Outlet />
